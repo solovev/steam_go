6cf5fa286b61294275809eda534d8d980f770968
Solovev <mrbroume@gmail.com>
2015-06-09 11:02:59 +0400

    Impl player summaries

---
+++ player.go
@@ -0,0 +1,55 @@
+package steam_auth
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type PlayerSummaries struct {
+	SteamId                  string `json:"steamid"`
+	CommunityVisibilityState int    `json:"communityvisibilitystate"`
+	ProfileState             int    `json:"profilestate"`
+	PersonaName              string `json:"personaname"`
+	LastLogOff               int    `json:"lastlogoff"`
+	ProfileUrl               string `json:"profileurl"`
+	Avatar                   string `json:"avatar"`
+	AvatarMedium             string `json:"avatarmedium"`
+	AvatarFull               string `json:"avatarfull"`
+	PersonaState             int    `json:"personastate"`
+
+	CommentPermission int    `json:"commentpermission"`
+	RealName          string `json:"realname"`
+	PrimaryClanId     string `json:"primaryclanid"`
+	TimeCreated       int    `json:"timecreated"`
+	LocCountryCode    int    `json:"loccountrycode"`
+	LocStateCode      int    `json:"locstatecode"`
+	LocCityId         int    `json:"loccityid"`
+	GameId            int    `json:"gameid"`
+	GameExtraInfo     string `json:"gameextrainfo"`
+	GameServerIp      string `json:"gameserverip"`
+}
+
+func GetPlayerSummaries(steamId, apiKey string) (*PlayerSummaries, error) {
+	url := fmt.Sprintf("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s", apiKey, steamId)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	type Result struct {
+		Response struct {
+			Players []PlayerSummaries `json:"players"`
+		} `json:"response"`
+	}
+	var data Result
+	if err := json.Unmarshal(body, &data); err != nil {
+		return nil, err
+	}
+	return &data.Response.Players[0], err
+}
